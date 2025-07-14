bc8279381b845d1b33b9324b23880cb31b15acf0
Phan Nguyen Khai <phan.nkhai05@gmail.com>
2025-07-14 11:41:26 +0200

    updated env var and added cjs build

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   dts: true,
   clean: true,
+  sourcemap: true,
+  outExtension: ({ format }) => ({
+    js: format === 'cjs' ? '.cjs' : '.mjs',
+  }),
 });
